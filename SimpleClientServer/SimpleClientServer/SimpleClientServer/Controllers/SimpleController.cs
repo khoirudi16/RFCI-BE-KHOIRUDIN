36607caa54c096e31406f0d443bd7b20da1979be
Muhammad Khoirudin <muhammad.khoirudin@xtremax.com>
2020-11-07 15:22:26 +0700

    Commit soal 1 dan 2

---
+++ SimpleClientServer/SimpleClientServer/SimpleClientServer/Controllers/SimpleController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleClientServer.Controllers
+{
+    public class SimpleController : Controller
+    {
+        // GET: Simple
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult CreateData(string message)
+        {
+            bool result = WriteLog(message);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public static bool WriteLog(string message)
+        {
+            try
+            {
+                string path = "D:\\Learning\\Csharp\\SimpleClientServer\\SimpleClientServer\\SimpleClientServer\\Log\\Message.txt";
+                string strMessage = String.Format("{0}", message);
+                FileStream objFilestream = new FileStream(path, FileMode.Append, FileAccess.Write);
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(strMessage);
+                objStreamWriter.Close();
+                objFilestream.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Sorting/Sorting/Sorting/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unSortedArray = { 4, 9, 7, 5, 8, 9, 3 };
+            int[] sortedArray = { 4, 9, 7, 5, 8, 9, 3 };
+            Array.Sort(sortedArray);
+
+            PrintingSwap(unSortedArray, sortedArray, unSortedArray.Length);
+        }
+
+        public static void PrintingSwap(int[] unSortedArray, int[] sortedArray, int n)
+        {
+            int temp;
+            int swaps = 0;
+            bool isLoop = false;
+            List<string> results = new List<string>();
+
+            while (!isLoop)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (unSortedArray[i] > unSortedArray[i + 1])
+                    {
+                        int left = unSortedArray[i + 1];
+                        int right = unSortedArray[i];
+
+                        temp = unSortedArray[i + 1];
+                        unSortedArray[i + 1] = unSortedArray[i];
+                        unSortedArray[i] = temp;
+                        swaps++;
+
+                        string arrString = string.Empty;
+
+                        foreach (int num in unSortedArray)
+                        {
+                            arrString += num + " ";
+                        }
+
+                        string rowString = "[" + left + "," + right + "] -> " + arrString;
+                        results.Add(rowString);
+                        break;
+                    }
+                    
+                }
+
+                isLoop = _Comparation(unSortedArray, sortedArray, n); //false
+
+            }
+
+            int no = 1;
+            foreach(string str in results)
+            {
+                Console.WriteLine(no + ". " + str);
+                no++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Jumlah swap: " + swaps);
+            Console.ReadKey();
+           
+        }
+
+        private static bool _Comparation(int[] unSortedArray, int[] sortedArray, int n)
+        {
+            bool result = true;
+            for(int i = 0; i < n; i++)
+            {
+                if(unSortedArray[i] != sortedArray[i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
